b8d17cd8ddeecfe9f1874ca94e8da690a5fb74bb
Saiful Islam <2001025@iot.bdu.ac.bd>
2024-04-02 23:49:27 +0600

    Create operator.cpp
    Day-25

---
+++ Day-25/operator.cpp
@@ -0,0 +1,9 @@
+/*
+BODMAS rule for Operation calculation.
+For Binary Operation:
+->Left to right Associativity
+{*,/,%}>{+,-}
+For Unary Operation:
+-Pre increment: ++a;(first increment of a,then print)
+-Post increment: a++;(first print the a.then increment)
+*/
